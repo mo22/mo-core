b2d73354c232ca61a1fbacefcad2a57fc10ac077
Moritz Moeller <mm@mxs.de>
2019-08-26 15:01:51 +0200

    ..

--- lib/PromiseTimeout.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function timeout<T>(promise: Promise<T>, ms: number): Promise<T>;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './Event';
 export * from './Future';
 export * from './Mutex';
-export * from './PromiseTimeout';
 export * from './Releaseable';
 export * from './Timer';
