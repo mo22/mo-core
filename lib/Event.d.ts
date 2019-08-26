8700188d9d2de9121408e612f9e46f275b9faab4
Moritz Moeller <mm@mxs.de>
2019-08-26 14:36:03 +0200

    ..

--- lib/Event.d.ts
+++ lib/Event.d.ts
@@ -3,38 +3,6 @@ export declare type EventUnsubscribeFunc = () => void;
 export declare type EventEmitFunc<T> = (value: T) => void;
 export declare type EventSubscribeFunc<T> = (emit: EventEmitFunc<T>) => EventUnsubscribeFunc;
 export declare type EventListenerFunc<T> = (value: T) => void;
-export interface NextObserver<T> {
-    closed?: boolean;
-    next: (value: T) => void;
-    error?: (err: any) => void;
-    complete?: () => void;
-}
-export interface ErrorObserver<T> {
-    closed?: boolean;
-    next?: (value: T) => void;
-    error: (err: any) => void;
-    complete?: () => void;
-}
-export interface CompletionObserver<T> {
-    closed?: boolean;
-    next?: (value: T) => void;
-    error?: (err: any) => void;
-    complete: () => void;
-}
-export declare type PartialObserver<T> = NextObserver<T> | ErrorObserver<T> | CompletionObserver<T>;
-export interface Unsubscribable {
-    unsubscribe(): void;
-}
-export interface Subscribable<T> {
-    subscribe(observer?: PartialObserver<T>): Unsubscribable;
-    /** @deprecated Use an observer instead of a complete callback */
-    subscribe(next: null | undefined, error: null | undefined, complete: () => void): Unsubscribable;
-    /** @deprecated Use an observer instead of an error callback */
-    subscribe(next: null | undefined, error: (error: any) => void, complete?: () => void): Unsubscribable;
-    /** @deprecated Use an observer instead of a complete callback */
-    subscribe(next: (value: T) => void, error: null | undefined, complete: () => void): Unsubscribable;
-    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Unsubscribable;
-}
 export declare class Event<T> implements AsyncIterable<T> {
     private _subscribe;
     private _unsubscribe?;
