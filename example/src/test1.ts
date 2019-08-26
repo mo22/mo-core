b6321a598e50ad97fafb1b62062373c20c4c864d
Moritz Moeller <mm@mxs.de>
2019-08-26 14:04:16 +0200

    ..

--- example/src/test1.ts
+++ example/src/test1.ts
@@ -1,1 +1,12 @@
 import { Timer } from 'mo-core';
+
+(async () => {
+
+  let c = 0;
+  for await (const _i of Timer.periodic(1000)) {
+    console.log('XXX', c);
+    c++;
+    if (c === 10) break;
+  }
+
+})().catch(console.error);
